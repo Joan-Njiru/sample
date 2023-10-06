1f78ccb124bb642668181aff050ceb6a7b2f3e7a
Joan-Njiru <joanwanjirunjiru@example.com>
2023-10-06 15:11:18 +0300

    initial

---
+++ frontend-dashboard/app/page.tsx
@@ -0,0 +1,14 @@
+
+import Sidebar from "./components/Sidebar";
+import Login from "./login/page";
+
+export default function Home() {
+  return(
+    <main>
+      {/* <Sidebar/> */}
+      <Login/>
+    </main>    
+                                                                                                                                        
+  )
+
+  }
